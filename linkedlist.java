22ec38b69af6c0f36b7d49d33811fabc7d623e3b
harshith <hanamalagundam@gmail.com>
2025-08-19 14:12:00 +0530

    Add files via upload

---
+++ linkedlist.java
@@ -0,0 +1,45 @@
+public class linkedlist{
+    static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    Node head = null;
+    public void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+    public void display() {
+        if (head == null) {
+            System.out.println("Linked list is empty.");
+            return;
+        }
+        Node current = head;
+        System.out.print("Linked List: ");
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+        list.insert(40);
+        list.display();
+    }
+}
+
